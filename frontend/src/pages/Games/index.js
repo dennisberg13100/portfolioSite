4f2d145495acab1aa2678ab61bf109de1c498b77
Dennis van den Berg <dennisvandenberg13100@gmail.com>
2020-05-13 13:08:52 +0200

    responsive homepage

--- frontend/src/pages/Games/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './styles.css';
-
-export default function Games(){
-    return (
-        <h1>Games</h1>
-    )
-}
--- frontend/src/pages/Home/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './styles.css';
-
-export default function Home(){
-    return (
-        <h1>Home</h1>
-    )
-}
